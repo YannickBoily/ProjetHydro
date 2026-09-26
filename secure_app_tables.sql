4620d9744700c7f7575e341aa758a0ff73d509cd
Yannick Boily <91081150+YannickBoily@users.noreply.github.com>
2026-09-25 20:08:58 -0400

    Delete secure_app_tables.sql

--- secure_app_tables.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE app_latest_outages ENABLE ROW LEVEL SECURITY;
-ALTER TABLE app_active_outages ENABLE ROW LEVEL SECURITY;
-ALTER TABLE app_daily_summary ENABLE ROW LEVEL SECURITY;
-ALTER TABLE app_data_quality_report ENABLE ROW LEVEL SECURITY;
-
-REVOKE ALL ON TABLE app_latest_outages FROM anon, authenticated;
-REVOKE ALL ON TABLE app_active_outages FROM anon, authenticated;
-REVOKE ALL ON TABLE app_daily_summary FROM anon, authenticated;
-REVOKE ALL ON TABLE app_data_quality_report FROM anon, authenticated;
